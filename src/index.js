40ec36fd838735d9dd498860041e24a34619ca2f
sedi <salehisediqe86@gmail.com>
2023-01-27 01:03:34 -0800

    Replaced the API from the apiweatherSheCodes,

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ return `${day} ${hours}:${minutes}`;
 function formatDay(timestamp){
 let date = new Date (timestamp * 1000);
 let day = date.getDay();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday" , "Thursday", "Friday", "Saturday"];
+let days = ["Sun", "Mon", "Tue", "Wed" , "Thu", "Fri", "Sat"];
 return days[day];
 }
 
@@ -29,11 +29,11 @@ forecast.forEach(function(forecastDay , index) {
     if(index < 6) { 
 forecastHTML =  forecastHTML + 
  ` <div class="col-2">
-                <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42" />
+                <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.daily[0].condition.icon}.png" alt="" width="42" />
         <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
-        <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temperature.maximum)}° </span>
+        <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temperature.minimum)}°</span>
         
     </div>
     </div>`;
@@ -46,7 +46,7 @@ forecastHTML =  forecastHTML +
 
 function getForecast(coordinates){
     let apiKey = "tb5f08b166ada0ab28a3f4o4dec6c3e0";
-    let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl =`https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
 }
 
@@ -64,15 +64,15 @@ function  displayTemperature(response){
      celsiusTemperature = response.data.temperature.current;
 
 temperatureElement.innerHTML =Math.round(response.data.temperature.current);
-cityElement.innerHTML = response.data.name;
+cityElement.innerHTML = response.data.city;
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = response.data.temperature.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.time * 1000);
-iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-iconElement.setAttribute("alt", response.data.weather[0].description);
+iconElement.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+iconElement.setAttribute("alt", response.data.condition.icon);
 
-getForecast(response.data.coord);
+getForecast(response.data.coordinates);
 }
 
 function search(city){
